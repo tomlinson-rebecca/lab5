068cf6c1437e6bbe01b08e22c46f3b868e60f37e
Rebecca <angryhamster14@gmail.com>
2019-02-07 11:49:09 -0800

    Add friends implemented, viewport scaling

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,16 @@ var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
 	// Your code goes here
+	name = request.query.name;
+	description = request.query.description;
+	var json = {
+		'name': name,
+		'description': description,
+		'imageURL': 'http://lorempixel.com/400/400/people/'
+	}
+	console.log(json);
+	data.friends.push(json);
+	response.render('index', {
+		"data": data.friends
+	});
  }
